6a941ded3f65d5cd900eca00fd2ee1bc656ebf63
ravi0424 <102217635+ravi0424@users.noreply.github.com>
2024-12-12 18:08:22 +0530

    Delete scripts directory

--- scripts/start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bas
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull ravikirangunnabattula/simple-python-flask-app
-
-# Run the Docker image as a container
-docker run -d -p 8000:5000 ravikirangunnabattula/simple-python-flask-app
