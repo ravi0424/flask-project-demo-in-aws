4bc19dc5e7437dba6d2648aef48ff1a7d4c4638d
ravi0424 <102217635+ravi0424@users.noreply.github.com>
2024-12-19 14:35:52 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -e 
+
 echo "Hi"
+
+# Get the container ID of the first running container
 containerid=$(docker ps | awk '{print $1}' | head -n 1)
-docker rm -f $containerid
+
+# Remove the container if it exists
+if [ -n "$containerid" ]; then
+    docker rm -f $containerid
+else
+    echo "No running containers found"
+fi
